e13d37a978758452053e8f5c69529aff42894a59
Jose Lezama <me@joseglego.io>
2023-07-09 22:30:17 -0400

    feat(Index): remove unused LastEpisode

--- src/app/(transition)/(root)/page.tsx
+++ src/app/(transition)/(root)/page.tsx
@@ -1,9 +1,6 @@
 import { Metadata } from 'next';
 
-import { API } from "@/api";
 import { TeapotLogo } from "@/components/teapot418";
-
-// import { LastEpisodeDocument } from "./lastEpisode.generated";
 
 export const metadata: Metadata = {
   title: "Status 418: I&apos;m a Teapot",
@@ -11,10 +8,7 @@ export const metadata: Metadata = {
     'Somos un Streaming y Podcast, de Tecnolog&iacute;a y Comunidad. Hecho con &hearts; por el equipo de JavaScript Chile.',
 };
 
-export default async function Home() {
-  const data = await API.LastEpisode();
-  const episodio = data.allEpisodio.at(0);
-
+export default function Home() {
   return (
     <main className="flex w-full max-w-5xl flex-col items-center justify-between px-6 pt-36 transition-all md:px-10 md:pt-44 xl:px-0 xl:pt-52">
       <div className="flex flex-col gap-4 pb-16 text-center items-center">
@@ -26,15 +20,6 @@ export default async function Home() {
           <span className="animate-gradient-text text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 ml-2">418</span>
         </h1>
         <h2 className="text-base text-slate-700 sm:text-2xl xl:text-3xl">Un Podcast de Tecnología, Web y Comunidad.</h2>
-      </div>
-      <div className="flex flex-col gap-4">
-        {episodio && (
-          <div>
-            <h2 className="text-2xl font-bold">Último episodio</h2>
-            <h3 className="text-xl font-bold">{episodio.titulo}</h3>
-            <p className="text-xl">{episodio.descripcion}</p>
-          </div>
-        )}
       </div>
     </main>
   );
