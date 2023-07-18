1d20edc338df71b55cc586a73bc7a4b9292a62e4
Jose Lezama <me@joseglego.io>
2023-07-18 11:44:58 -0400

    feat(Index): include social Links and Placeholders

--- src/app/(transition)/(root)/page.tsx
+++ src/app/(transition)/(root)/page.tsx
@@ -1,9 +1,17 @@
 import { Metadata } from 'next';
+import Link from "next/link";
 
 import { TeapotLogo } from "@/components/teapot418";
+import { ApplePodcast } from "@/components/icons/apple_podcast";
+import { GooglePodcast } from "@/components/icons/google_podcast";
+import { Spotify } from "@/components/icons/spotify";
+import { Youtube } from '@/components/icons/youtube';
+import { LinkedIn } from '@/components/icons/linkedin';
+import { Twitch } from '@/components/icons/tiwtch';
+import { Facebook } from '@/components/icons/facebook';
 
 export const metadata: Metadata = {
-  title: "Status 418: I&apos;m a Teapot",
+  title: "Status 418: Im a Teapot",
   description:
     'Somos un Streaming y Podcast, de Tecnolog&iacute;a y Comunidad. Hecho con &hearts; por el equipo de JavaScript Chile.',
 };
@@ -13,22 +21,70 @@ export default function Home() {
     <>
       <div className="
         absolute w-full h-full overflow-hidden
-        before:absolute before:-left-[0%] before:-top-[0%] lg:before:-left-[5%] lg:before:-top-[5%] before:-z-10 before:h-1/5	before:w-1/5 before:bg-gradient-radial before:from-purple-400 before:from-purple-400 before:blur-3xl before:opacity-80 lg:before:opacity-40 before:rounded-full
+        hover:before:absolute before:-left-[0%] before:-top-[0%] lg:before:-left-[5%] lg:before:-top-[5%] before:-z-10 before:h-1/5	before:w-1/5 before:bg-gradient-radial before:from-purple-400 before:to-purple-400 before:blur-3xl before:opacity-80 lg:before:opacity-40 before:rounded-full
         after:absolute after:-right-[0%] after:-bottom-[0%] lg:after:-right-[5%] lg:after:-bottom-[5%] after:-z-10 after:h-1/5	after:w-1/5 after:bg-gradient-radial after:from-pink-600 after:to-pink-600 after:blur-3xl after:opacity-80 lg:after:opacity-40 after:rounded-full
       " />
       <main className="flex items-center justify-center h-[calc(100vh_-_100px)] max-w-5xl px-6 transition-all">
         <div className="
-          relative flex flex-col gap-4 pb-16 text-center items-center
-          before:animate-gradient-svg before:absolute before:left-[10%] before:top-[10%] before:-z-10 before:h-[80%] before:w-[80%] before:opacity-30	before:bg-gradient-radial before:from-purple-400 before:to-pink-600 before:blur-3xl before:rounded-full
+          relative flex flex-col gap-4 text-center items-center
         ">
-          <div className="h-32 w-32 sm:h-64 sm:w-64 animate-gradient-svg from-purple-400 to-pink-600">
-            <TeapotLogo />
+          <div className="
+            relative flex flex-col gap-4 text-center items-center
+            before:animate-gradient-svg before:absolute before:left-[10%] before:top-[10%] before:-z-10 before:h-[80%] before:w-[80%] before:opacity-30	before:bg-gradient-radial before:from-purple-400 before:to-pink-600 before:blur-3xl before:rounded-full
+          ">
+            <div className="h-32 w-32 sm:h-64 sm:w-64 animate-gradient-svg from-purple-400 to-pink-600">
+              <TeapotLogo />
+            </div>
+            <h1 className="text-5xl font-extrabold shadow-slate-900 transition-all sm:text-7xl xl:text-8xl">
+              <span>Status</span>
+              <span className="animate-gradient-text text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 ml-2">418</span>
+            </h1>
           </div>
-          <h1 className="text-5xl font-extrabold shadow-slate-900 transition-all sm:text-7xl xl:text-8xl">
-            <span>Status</span>
-            <span className="animate-gradient-text text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600 ml-2">418</span>
-          </h1>
-          <h2 className="text-base text-slate-500 sm:text-2xl xl:text-3xl">Un Podcast de Tecnología, Web y Comunidad.</h2>
+          <h2 className="text-base text-slate-300 sm:text-2xl xl:text-3xl mb-4">Un Podcast de Tecnología, Web y Comunidad.</h2>
+          <div className="">
+            <h3 className="text-base text-slate-400 sm:text-lg xl:text-xl">Escucha en vivo</h3>
+            <div className="h-8 flex gap-4 gap-x-8 justify-center my-4 text-slate-300">
+              <Link
+                className="
+                  h-8 w-8 cursor-pointer hover:no-underline hover:text-red-600 relative
+                  hover:before:animate-gradient-svg hover:before:absolute hover:before:-left-[10%] hover:before:-top-[10%] hover:before:-z-100 hover:before:h-10 hover:before:w-10 hover:before:bg-gradient-radial hover:before:from-red-400 hover:before:to-red-600 hover:before:blur hover:before:rounded-full hover:before:opacity-50
+                "
+                target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/@javascriptchile">
+                <Youtube />
+              </Link>
+              <Link
+                className="
+                  h-8 w-8 cursor-pointer hover:no-underline hover:text-purple-600 relative
+                  hover:before:animate-gradient-svg hover:before:absolute hover:before:-left-[10%] hover:before:-top-[10%] hover:before:-z-100 hover:before:h-10 hover:before:w-10 hover:before:bg-gradient-radial hover:before:from-purple-400 hover:before:to-purple-600 hover:before:blur hover:before:rounded-full hover:before:opacity-50
+                "
+                target="_blank" rel="noopener noreferrer" href="https://www.twitch.tv/javascriptchile">
+                <Twitch />
+              </Link>
+              <Link
+                className="
+                  h-8 w-8 cursor-pointer hover:no-underline hover:text-sky-600 relative
+                  hover:before:animate-gradient-svg hover:before:absolute hover:before:-left-[10%] hover:before:-top-[10%] hover:before:-z-100 hover:before:h-10 hover:before:w-10 hover:before:bg-gradient-radial hover:before:from-sky-400 hover:before:to-sky-600 hover:before:blur hover:before:rounded-full hover:before:opacity-50
+                "
+                target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/company/javascript-chile/events/">
+                <LinkedIn />
+              </Link>
+              <Link
+                className="
+                  h-8 w-8 cursor-pointer hover:no-underline hover:text-blue-600 relative
+                  hover:before:animate-gradient-svg hover:before:absolute hover:before:-left-[10%] hover:before:-top-[10%] hover:before:-z-100 hover:before:h-10 hover:before:w-10 hover:before:bg-gradient-radial hover:before:from-blue-400 hover:before:to-blue-600 hover:before:blur hover:before:rounded-full hover:before:opacity-50
+                "
+                target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/groups/JavascriptChile/events">
+                <Facebook />
+              </Link>
+            </div>
+            <h3 className="text-base text-slate-400 sm:text-lg xl:text-xl mt-8">Escucha los capitulos anteriores</h3>
+            <div className="h-8 flex gap-4 gap-x-8 justify-center my-4 text-slate-300">
+              <ApplePodcast />
+              <GooglePodcast />
+              <Spotify />
+            </div>
+
+          </div>
         </div>
       </main>
     </>
