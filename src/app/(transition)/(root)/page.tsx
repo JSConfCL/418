5ca865f04ce7b9c6c37e3d212cd54ba3bda9073a
Jose Lezama <me@joseglego.io>
2023-09-10 23:57:31 -0300

    feat: show better meta-data with latest episodes and images

--- src/app/(transition)/(root)/page.tsx
+++ src/app/(transition)/(root)/page.tsx
@@ -1,19 +1,49 @@
-import { Metadata } from "next";
+import type { Metadata } from "next";
 
 import { API } from "@/api";
+
+import ClientPage from "./client-page";
+
+import { seo } from "@/utils/data";
+
 import { Episode } from "@/api/types";
+import { formatDate } from "@/utils/dates";
 
-import ClientPage from './client-page';
+export async function generateMetadata(): Promise<Metadata> {
+  const data = await API.LatestEpisodes();
+  const latestEpisodes = data?.allEpisode;
+  const latestOrNextEpisode = latestEpisodes[0];
 
-export const metadata: Metadata = {
-  title: "Status 418: Im a Teapot",
-  description:
-    "Somos un Streaming y Podcast, de Tecnolog&iacute;a y Comunidad. Hecho con &hearts; por el equipo de JavaScript Chile.",
-};
+  let description = seo.description;
+  if (
+    latestOrNextEpisode.date &&
+    new Date(latestOrNextEpisode.date) > new Date()
+  ) {
+    description += ` Ve en vivo nuestro próximo Episodio el ${formatDate(
+      latestOrNextEpisode.date
+    )}.`;
+    description += ` Chequea nuestro último episodio: #${latestEpisodes[1].episode} - ${latestEpisodes[1].title}`;
+  } else {
+    description += ` Chequea nuestro último episodio: #${latestOrNextEpisode.episode} - ${latestOrNextEpisode.title}`;
+  }
+
+  return {
+    ...seo,
+    description,
+    openGraph: {
+      ...seo.openGraph,
+      description,
+    },
+    twitter: {
+      ...seo.twitter,
+      description,
+    },
+  };
+}
 
 export default async function Page() {
-  const data = await API.LatestEpisodes()
-  const latestEpisodes = data?.allEpisode
+  const data = await API.LatestEpisodes();
+  const latestEpisodes = data?.allEpisode;
 
-  return <ClientPage latestEpisodes={latestEpisodes as Episode[]} />
+  return <ClientPage latestEpisodes={latestEpisodes as Episode[]} />;
 }
