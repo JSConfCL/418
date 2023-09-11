00ffb2b36af1cfca4281423648e01bf966ec5eb0
Jose Lezama <me@joseglego.io>
2023-09-10 23:57:18 -0300

    core: use IconLink instead of Manually copy-paste icon links

--- src/app/(transition)/(root)/page.tsx
+++ src/app/(transition)/(root)/page.tsx
@@ -1,18 +1,13 @@
 import { Metadata } from "next";
-import Link from "next/link";
+
+import classNames from "classnames";
+
+import { StreamLink } from "@/components/StreamLink/StreamLink";
 
 import { TeapotLogo } from "@/components/teapot418";
-import { ApplePodcast } from "@/components/icons/apple_podcast";
-import { GooglePodcast } from "@/components/icons/google_podcast";
-import { Spotify } from "@/components/icons/spotify";
-import { Youtube } from "@/components/icons/youtube";
-import { LinkedIn } from "@/components/icons/linkedin";
-import { Twitch } from "@/components/icons/tiwtch";
-import { Facebook } from "@/components/icons/facebook";
 import { Question } from "@/components/icons/question";
 import { Rocket } from "@/components/icons/rocket";
 import { Guest } from "@/components/icons/guest";
-import classNames from "classnames";
 
 export const metadata: Metadata = {
   title: "Status 418: Im a Teapot",
@@ -55,68 +50,18 @@ export default function Home() {
               Escucha en vivo
             </h3>
             <div className="my-4 flex h-8 justify-center gap-4 gap-x-8 text-slate-300">
-              <Link
-                // eslint-disable-next-line tailwindcss/no-custom-classname, tailwindcss/enforces-negative-arbitrary-values
-                className="
-                  hover:before:-z-100 relative h-8 w-8 cursor-pointer hover:text-red-600
-                  hover:no-underline hover:before:absolute hover:before:-left-[10%] hover:before:-top-[10%] hover:before:h-10 hover:before:w-10 hover:before:animate-gradient-svg hover:before:rounded-full hover:before:bg-gradient-radial hover:before:from-red-400 hover:before:to-red-600 hover:before:opacity-50 hover:before:blur
-                "
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://www.youtube.com/@javascriptchile"
-              >
-                <Youtube />
-              </Link>
-              <Link
-                // eslint-disable-next-line tailwindcss/no-custom-classname, tailwindcss/enforces-negative-arbitrary-values
-                className="
-                  hover:before:-z-100 relative h-8 w-8 cursor-pointer hover:text-purple-600
-                  hover:no-underline hover:before:absolute hover:before:-left-[10%] hover:before:-top-[10%] hover:before:h-10 hover:before:w-10 hover:before:animate-gradient-svg hover:before:rounded-full hover:before:bg-gradient-radial hover:before:from-purple-400 hover:before:to-purple-600 hover:before:opacity-50 hover:before:blur
-                "
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://www.twitch.tv/javascriptchile"
-              >
-                <Twitch />
-              </Link>
-              <Link
-                // eslint-disable-next-line tailwindcss/no-custom-classname, tailwindcss/enforces-negative-arbitrary-values
-                className="
-                  hover:before:-z-100 relative h-8 w-8 cursor-pointer hover:text-sky-600
-                  hover:no-underline hover:before:absolute hover:before:-left-[10%] hover:before:-top-[10%] hover:before:h-10 hover:before:w-10 hover:before:animate-gradient-svg hover:before:rounded-full hover:before:bg-gradient-radial hover:before:from-sky-400 hover:before:to-sky-600 hover:before:opacity-50 hover:before:blur
-                "
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://www.linkedin.com/company/javascript-chile/events/"
-              >
-                <LinkedIn />
-              </Link>
-              <Link
-                // eslint-disable-next-line tailwindcss/no-custom-classname, tailwindcss/enforces-negative-arbitrary-values
-                className="
-                  hover:before:-z-100 relative h-8 w-8 cursor-pointer hover:text-blue-600
-                  hover:no-underline hover:before:absolute hover:before:-left-[10%] hover:before:-top-[10%] hover:before:h-10 hover:before:w-10 hover:before:animate-gradient-svg hover:before:rounded-full hover:before:bg-gradient-radial hover:before:from-blue-400 hover:before:to-blue-600 hover:before:opacity-50 hover:before:blur
-                "
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://www.facebook.com/groups/JavascriptChile/events"
-              >
-                <Facebook />
-              </Link>
+              <StreamLink href="https://www.youtube.com/@javascriptchile" />
+              <StreamLink href="https://www.twitch.tv/javascriptchile" />
+              <StreamLink href="https://www.linkedin.com/company/javascript-chile/events/" />
+              <StreamLink href="https://www.facebook.com/groups/JavascriptChile/events" />
             </div>
             <h3 className="mt-10 text-base text-slate-400 sm:text-lg xl:text-xl">
               Escucha los episodios anteriores
             </h3>
             <div className="my-4 flex h-8 justify-center gap-4 gap-x-8 text-slate-300">
-              <span className="h-8 w-8 cursor-not-allowed">
-                <ApplePodcast />
-              </span>
-              <span className="h-8 w-8 cursor-not-allowed">
-                <GooglePodcast />
-              </span>
-              <span className="h-8 w-8 cursor-not-allowed">
-                <Spotify />
-              </span>
+              <StreamLink icon="apple" />
+              <StreamLink icon="google" />
+              <StreamLink icon="spotify" />
             </div>
             <h3 className="mt-10 text-base text-slate-400 sm:text-lg xl:text-xl">
               Nuestro Podcast
